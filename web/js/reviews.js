37b0b6c9f510f8d0c390743350531ab06effae0f
MuksithMRA <muksithmra@gmail.com>
2023-12-23 13:19:32 +0530

    Added reviews

--- web/js/reviews.js
+++ web/js/reviews.js
@@ -9,7 +9,7 @@ const reviewList = [
     job: "Web Developer",
     img: "https://t4.ftcdn.net/jpg/02/91/24/27/360_F_291242770_z3XC7rJB1Mvc5jVMsEY9Dx2xMrX4sxUi.jpg",
     review:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, voluptatum.",
+      "Unforgettable Adventure! From start to finish, Daybreak exceeded our expectations.",
   },
   {
     id: 2,
@@ -19,7 +19,7 @@ const reviewList = [
     job: "Graphic Designer",
     img: "./images/review2.jpg",
     review:
-      "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
+      "Seamless organization, friendly guides, and a truly memorable experience. Highly recommend!",
   },
   {
     id: 3,
@@ -29,7 +29,57 @@ const reviewList = [
     job: "Software Engineer",
     img: "./images/review3.jpg",
     review:
-      "Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "Outstanding customer service, and the complimentary beverages added a delightful touch to the experience.",
+  },
+  {
+    id: 4,
+    profilePic:
+      "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    name: "John Doe",
+    job: "Web Developer",
+    img: "https://t4.ftcdn.net/jpg/02/91/24/27/360_F_291242770_z3XC7rJB1Mvc5jVMsEY9Dx2xMrX4sxUi.jpg",
+    review:
+      "Well-organized private tours, comfortable travel, and the avoidance of cramped buses—this tour company knows how to create a seamless and spacious experience",
+  },
+  {
+    id: 5,
+    profilePic:
+      "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    name: "Alice Smith",
+    job: "Graphic Designer",
+    img: "./images/review2.jpg",
+    review:
+      "The traveler's guide was a brilliant addition, offering fascinating insights into each destination. A perfect blend of fun and education—highly recommended!",
+  },
+  {
+    id: 6,
+    profilePic:
+      "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    name: "Bob Johnson",
+    job: "Software Engineer",
+    img: "./images/review3.jpg",
+    review:
+      "Outstanding customer service from booking to the trip's end. Responsive, accommodating, and dedicated staff!",
+  },
+  {
+    id: 7,
+    profilePic:
+      "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    name: "Alice Smith",
+    job: "Graphic Designer",
+    img: "./images/review2.jpg",
+    review:
+      "Well-organized, comfortable, and stress-free. This tour company knows how to create a seamless experience. Highly impressed!",
+  },
+  {
+    id: 8,
+    profilePic:
+      "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    name: "Bob Johnson",
+    job: "Software Engineer",
+    img: "./images/review3.jpg",
+    review:
+      "Unique and authentic encounters set this tour apart. Explored hidden gems and connected with the local culture. A truly special journey.",
   },
 ];
 
